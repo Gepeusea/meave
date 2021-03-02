df4a6b6e0f1350382f092a8692d1e25cc858b1f1
gepeusea <ulfinka@gmail.com>
2021-03-02 09:39:12 +0300

    Небольшие исправления

--- dateanalysis.py
+++ dateanalysis.py
@@ -6,10 +6,10 @@ todaysDate = str(dateime.date.today())
 def pushFunction(text):
 	return(text)
 
-flag1 == False
-flag2 == False
-flag3 == False
-flag4 == False
+flag1 == True
+flag2 == True
+flag3 == True
+flag4 == True
 
 def flagController():
 	pass
@@ -17,12 +17,12 @@ def flagController():
 #выводит напоминание, если пользователь сегодня не выполнил ни одного задания
 def unproductiveDay(calendarData):
 	check = True
-	if int(now.hour) == 20 and flag1 == False:
+	if int(now.hour) == 20 and flag1:
 		if calendarData[todaysDate[:4]][todaysDate[5:7]][todaysDate[8:]][0] > 0:
 			check = False
 		if check:
 			pushFunction('Сегодня вы не выполнили ни одной задачи, но еще не поздно это изменить')
-		flag1 == True
+			flag1 == False
 
 def calculations(calendarData,daysInLastMonth,counter, returnLen = None):
 	attempt = int(todaysDate[8:])-counter
@@ -38,28 +38,29 @@ def calculations(calendarData,daysInLastMonth,counter, returnLen = None):
 #выводит напоминание в случае, если всю последнюю неделю пользователь был непродуктивен
 def unproductiveWeek(calendarData,daysInLastMonth):
 	check = True
-	if int(now.hour) == 12 and flag2 == False:
+	if int(now.hour) == 12 and flag2:
 		for i in range(7):
 			if calculations(calendarData,daysInLastMonth,i) > 3:
 				check = False
 		if check:
 			pushFunction('На этой неделе у вас наблюдалась низкая продуктивность, давайте исправим это сегодня')
-		flag2 == True
+			flag2 == False
 
 #выводит напоминание когда пользователь поддерживает высокую продуктивность в течение долгого периода
 def hightActivity(calendarData,daysInLastMonth):
 	check = True
-	if int(now.hour) == 12 and flag2 == False:
+	if int(now.hour) == 12 and flag3:
 		for i in range(7):
 			if calculations((calendarData,daysInLastMonth,i)+1) < 4:
 				check = False
 		if check:
 			pushFunction('Последнюю неделю вы предерживались режима высокой продуктивности. Что думаете на счет разгрузочного дня?')
-		flag2 == True
+			flag3 == False
 
 #напоминание о невыполненных задачах
 def outstandingTask(calendarData,daysInLastMonth):
 	check = False
-	if int(now.hour) == 10 and flag2 == False:
-		if calculations(calendarData,daysInLastMonth,1) < (calendarData,daysInLastMonth,1, True)
-		pushFunction('У вас есть невыполненные задачи за прошлый день, но вы можете расправиться с ними сегодня')
+	if int(now.hour) == 10 and flag4:
+		if calculations(calendarData,daysInLastMonth,1) < (calendarData,daysInLastMonth,1, True):
+			pushFunction('У вас есть невыполненные задачи за прошлый день, но вы можете расправиться с ними сегодня')
+			flag4 == False
