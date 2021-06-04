a3c4f5672006199c0450f2851fa2dc406f4d69c3
gepeusea <ulfinka@gmail.com>
2021-06-04 20:06:02 +0300

    Теперь нажатие на чекбокс меняет и сохраняет флаг

--- OpenTaskManager.py
+++ OpenTaskManager.py
@@ -11,10 +11,12 @@ LastStateRole = QtCore.Qt.UserRole
 
 class ViewTaskIn(Qt.QWidget):
 
-    def __init__(self, newExemplar):
+    def __init__(self, date, tasks, newExemplar):
         super().__init__()
         layout = Qt.QHBoxLayout(self)
-        self.tasks = []
+        self.tasks = tasks
+        print('this is tasks: ', tasks)
+        self.date = date
         self.table = Qt.QTableWidget()
         self.table.setRowCount(3)
         self.table.setColumnCount(2)
@@ -31,12 +33,12 @@ class ViewTaskIn(Qt.QWidget):
         layout.addWidget(self.table)
         self.table.cellChanged.connect(self.onCellChanged)
 
-    def create_1(self, text: str, level: int, is_checked: bool, row: int, date: datetime.datetime):
+    def create_1(self, row: int, date: datetime.datetime):
         item = QtWidgets.QTableWidgetItem()
-        item.setText(text)
+        item.setText(self.tasks[row]['note'])
         #item.setText('<html><head/><body><p align="center"><span style=" font-size:36pt;">{text}</span></p></body></html>')
         item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
-        if is_checked:
+        if self.tasks[row]['flag']:
             item.setCheckState(QtCore.Qt.CheckState(2))
         else:
             item.setCheckState(QtCore.Qt.CheckState(0))
@@ -46,7 +48,6 @@ class ViewTaskIn(Qt.QWidget):
         self.table.setItem(row, 0, item)
         self.table.resizeColumnsToContents()
         #self.table.setSizeHint(QSize(320, 200))
-        self.tasks.append([date, text, level, is_checked])
 
         btn_edit = QtWidgets.QPushButton('✏️')
         btn_edit.setGeometry(200, 150, 100, 40)
@@ -65,15 +66,11 @@ class ViewTaskIn(Qt.QWidget):
         self.task.show()
 
     def onCellChanged(self, row, column):
-        item = self.table.item(row, column)
-        lastState = item.data(LastStateRole)
-        currentState = item.checkState()
-        if currentState != lastState:
-            if currentState == QtCore.Qt.Checked:
-                print(f'now checked in {row}')
-            else:
-                print(f'now unchecked in {row}')
-            item.setData(LastStateRole, currentState)
+         item = self.table.item(row, column)
+         lastState = item.data(LastStateRole)
+         currentState = item.checkState()
+         if currentState != lastState:
+            self.newExemplar.changeFlag(self.date, row)
 
 
 class TaskManager(QtWidgets.QMainWindow):
@@ -84,7 +81,7 @@ class TaskManager(QtWidgets.QMainWindow):
         super(TaskManager, self).__init__(*args, **kwargs)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        container = ViewTaskIn(newExemplar)
+        container = ViewTaskIn(date, tasks, newExemplar)
         print('at sec',type(tasks))
         print(tasks)
         if tasks != None:
@@ -94,7 +91,7 @@ class TaskManager(QtWidgets.QMainWindow):
             #for task in iterable_tasks:
             for j in range(len(tasks)):
                 print('ura')
-                container.create_1(tasks[j]['note'], tasks[j]['grade'], tasks[j]['flag'], i, date)
+                container.create_1(i, date)
                 i += 1
         if date:
             self.ui.current_date.setText(f'<html><head/><body><p align="center"><span style=" font-size:36pt;">{str(date)}</span></p></body></html>')
